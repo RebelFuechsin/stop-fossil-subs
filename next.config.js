423935c9f4574c50bb2cab0ee66eff3a20fc0ba0
Kristina Preusz <kristina.roddewig@soundcloud.com>
2023-10-17 01:55:05 +0200

    set in production only

--- next.config.js
+++ next.config.js
@@ -1,10 +1,18 @@
 /** @type {import('next').NextConfig} */
-  const nextConfig = {
-    basePath: '/stop-fossil-subs',
+
+const isProd = process.env.NODE_ENV === 'production'
+
+const nextConfig = {
+    basePath: isProd ? '/stop-fossil-subs' : '',
+    output: 'export',
+    distDir: 'dist',
     reactStrictMode: true,
     typescript: {
       ignoreBuildErrors: true,
     },
+    images: {
+      unoptimized: true,
+    }
   }
 
   module.exports = nextConfig
